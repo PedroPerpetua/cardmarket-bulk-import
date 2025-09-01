2fb810b03aae6d5cadf13d2e7dd60a5ba140b329
PedroPerpetua <pedro.perpetua@sapo.pt>
2025-09-02 00:40:35 +0100

    Fixed CSS issue

--- src/entrypoints/popup/main.tsx
+++ src/entrypoints/popup/main.tsx
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-import './style.css';
+/* eslint-disable import/order */
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+/* eslint-enable import/order */
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
